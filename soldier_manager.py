bb5609820490095ca39b3f372d4edaebbd22f791
aharon weber <a.w770770@gmail.com>
2026-05-20 11:18:22 +0300

    soldier_manager-function

---
+++ soldier_manager.py
@@ -0,0 +1,20 @@
+def add_soldier(soldier_id: int, name: str) -> None:
+    """Adds a soldier to the system."""
+    pass
+
+def remove_soldier(soldier_id: int) -> None:
+    """Removes a soldier from the system."""
+    pass
+
+
+def get_all_soldiers() -> list:
+    """Returns a list of all soldiers in the system ."""
+    pass
+
+def update_duty_status(soldier_id: int, duty_name: str, new_status: str) -> None:
+    """Updates the duty status of a soldier."""
+    pass
+
+def get_soldier_duties(soldier_id: int) -> list:
+    """Returns a list of duties assigned to a soldier."""
+    pass
