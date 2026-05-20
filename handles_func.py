c9955c77023c9c744bb871387e2e23346f2217c6
aharon weber <a.w770770@gmail.com>
2026-05-20 12:33:18 +0300

    fix handles_func imports and add loop

--- handles_func.py
+++ handles_func.py
@@ -1,25 +1,22 @@
-from os import name
-
 from data import data
 from menu import show_menu, get_user_choice
 from soldier_manager import add_soldier, remove_soldier, get_all_soldiers, update_duty_status, get_soldier_duties
-from input import input_id, input_name  
-from utils import is_valid_day, is_valid_id, is_valid_name, is_valid_status, find_soldier_by_id, find_soldier_by_name, soldier_has_duty is_valid_id        
+from input import input_id, input_name
+from utils import is_valid_day, is_valid_id, is_valid_name, is_valid_status, find_soldier_by_id, find_soldier_by_name, soldier_has_duty
 
 
 
 def handle_add_soldier() -> None:
     """Handles the logic for adding a soldier."""
-    flag = True
+    flag = False
     while not flag:
-            soldier_id = input_id()
-            if is_valid_id(soldier_id):
+        soldier_id = input_id()
+        if is_valid_id(soldier_id):
+            name = input_name()
+            if is_valid_name(name):
                 flag = True
-                name = input_name()
-                if is_valid_name(name):
-                    flag = True
     add_soldier(soldier_id, name)
-    print(f"Soldier {name} with ID {soldier_id} added successfully.")   
+    print(f"Soldier {name} with ID {soldier_id} added successfully.")
            
     
 
